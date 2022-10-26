b113e5dd26ce14c6836ebb3f11ab29279f542335
Prateek Varma <prtk.varma@gmail.com>
2022-10-26 15:51:13 -0600

    Remove item from cart method implemented, and hooked to configureStore()

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -23,6 +23,19 @@ const cartSlice = createSlice({
                 existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
         },
-        removeItemFromCart() {},
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter((item) => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        },
     }
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
